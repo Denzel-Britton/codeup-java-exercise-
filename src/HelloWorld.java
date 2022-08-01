6b429041c56cb24779ca3cca9699fe231597acec
Denzel-Britton <denzel.britton573@gmail.com>
2022-08-01 16:53:00 -0500

    finished HelloWorld exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.sql.Statement;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        int myfavoriteNumber = 7;
+//        System.out.println("this is my favorite num " + myfavoriteNumber);
+//        String myString= "This is my string varible";
+//        System.out.println(myString);
+//        myString = String.valueOf('c');
+//        System.out.println(myString);
+//        double myString = 3.14159;
+//        System.out.println(myString);
+//        double myNumber = 3.14;
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String class
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//         x += 5;
+//         System.out.println(x);
+//        int x = 3,y= 4 ;
+//        y *=x;
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x /=y;
+//        y-=x;
+//        System.out.println(x);
+//        System.out.println(y);
+        byte b;
+        b=(byte)129;
+        System.out.println(b);
+
+    }
 }
